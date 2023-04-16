b5be5f5198c4ff3c07889c047a3d14442fc907ed
ILLIAS LEV <yrist.list@gmail.com>
2023-04-15 22:47:28 -0700

    Ошибка: перемещение к элементу не работает

--- test_vodafone.py
+++ test_vodafone.py
@@ -24,31 +24,31 @@ def test_site_name_text(driver):
     blok_cooke_v_futere = driver.find_element(By.CSS_SELECTOR, locators.cooke_block_in_footer)
     knopka_ok_v_futere_cookies_div = driver.find_element(By.CSS_SELECTOR, locators.cooke_ok_button)
     actions = ActionChains(driver)
-    if blok_cooke_v_futere:
-        print("В футере был куки вопрос")
-        actions.move_to_element(knopka_ok_v_futere_cookies_div).click(knopka_ok_v_futere_cookies_div)
+    # if blok_cooke_v_futere:
+    #     print("В футере был куки вопрос")
+    #     actions.move_to_element(knopka_ok_v_futere_cookies_div).click(knopka_ok_v_futere_cookies_div)
 
 
     time.sleep(2)
     actions.move_to_element(element_nije_placeholdera)
     time.sleep(2)
-    placeholder_vibor_nomera.click()
-    time.sleep(2)
-    vibor_nomera_1_BACKSPACE = driver.find_element(By.CSS_SELECTOR, locators.number_placeholder).send_keys(Keys.BACKSPACE)
-    time.sleep(2)
-    vibor_nomera_1_BACKSPACE
-    time.sleep(2)
-    vibor_nomera_1_BACKSPACE
-    time.sleep(2)
-    vibor_nomera_1_BACKSPACE
-    time.sleep(2)
-    vibor_nomera_1_BACKSPACE
-    time.sleep(2)
-    vibor_nomera_1_BACKSPACE
-    time.sleep(2)
-    vibor_nomera_1_BACKSPACE
-    time.sleep(2)
-    driver.find_element(By.CSS_SELECTOR, locators.number_placeholder).send_keys('99')
+    # placeholder_vibor_nomera.click()
+    # time.sleep(2)
+    # vibor_nomera_1_BACKSPACE = driver.find_element(By.CSS_SELECTOR, locators.number_placeholder).send_keys(Keys.BACKSPACE)
+    # time.sleep(2)
+    # vibor_nomera_1_BACKSPACE
+    # time.sleep(2)
+    # vibor_nomera_1_BACKSPACE
+    # time.sleep(2)
+    # vibor_nomera_1_BACKSPACE
+    # time.sleep(2)
+    # vibor_nomera_1_BACKSPACE
+    # time.sleep(2)
+    # vibor_nomera_1_BACKSPACE
+    # time.sleep(2)
+    # vibor_nomera_1_BACKSPACE
+    # time.sleep(2)
+    # driver.find_element(By.CSS_SELECTOR, locators.number_placeholder).send_keys('99')
     time.sleep(2)
     # assert expected_site_name_H1 == site_name_H1_text
     # print('   #print = ', expected_site_name_H1, ' = ', site_name_H1_text)
